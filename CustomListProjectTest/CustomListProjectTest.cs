b120371e8414901c32e3ec5567460ee5297efd98
Sarah Steiner <steiner.sa96@gmail.com>
2019-03-12 15:39:33 -0500

    Add tests written. Next step is to work on Add method.

---
+++ CustomListProjectTest/CustomListProjectTest.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomListProjectTest;
+using System.Collections.Generic;
+
+namespace CustomListProjectTest
+{
+    [TestClass]
+    public class CustomListProjectTest
+    {
+        [TestMethod]
+        public void Add_AddToEmptyList_AddsItemToIndexZero()
+        {
+            // Arrange
+            List<int> test = new List<int>();
+            int thingToAdd = 10;
+            int expected = 10;
+            int actual;
+
+            // Act
+            test.Add(thingToAdd);
+            actual = test[0];
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Add_EmptyList_CountIncrements()
+        {
+           // Arrange
+           List<int> test = new List<int>();
+           int thingToAdd = 10;
+           int expected = 1;
+           int actual;
+
+           // Act
+           actual = test.Count;
+
+           // Assert
+           Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Add_ListCountThree_AddsToIndexThree()
+        {
+            // Arrange
+            List<int> test = new List<int>();
+            int thingToAdd = 10;
+            test.Add(thingToAdd);
+            test.Add(thingToAdd);
+            test.Add(thingToAdd);
+            int expected = 10;
+            int actual;
+
+            // Act
+            test.Add(thingToAdd);
+            actual = test[3];
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Add_ListCountOne_AddsToIndexThree()
+        {
+            // Arrange
+            List<int> test = new List<int>();
+            int thingToAdd = 10;
+            test.Add(thingToAdd);
+            int expected = 40;
+            int actual;
+
+            // Act
+            test.Add(20);
+            test.Add(30);
+            test.Add(40);
+            actual = test[3];
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Add_ListCountFour_AddsToIndexFour() // Array is full... need to make a new one
+        {
+            // Arrange
+            List<int> test = new List<int>();
+            int thingToAdd = 10;
+            test.Add(thingToAdd);
+            test.Add(thingToAdd);
+            test.Add(thingToAdd);
+            test.Add(thingToAdd);
+            int expected = 10;
+            int actual;
+
+            // Act
+            test.Add(thingToAdd);
+            actual = test[4];
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
---
+++ CustomListProjectTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CustomListProjectTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("CustomListProjectTest")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e69d0acd-91b8-457f-81ed-5b329926b24a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
